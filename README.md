b83c6a1c3db026eceaab16a21f1798daa66804a2
dcastellonc <danielacastellonc@outlook.com>
2023-03-23 17:01:53 -0400

    final commit

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
 # Crowdfunding_ETL
+Instructions
+The instructions for this mini project are divided into the following subsections:
+- Create the Category and Subcategory DataFrames
+- Create the Campaign DataFrame
+- Create the Contacts DataFrame
+- Create the Crowdfunding Database
+
+
+* Create the Category and Subcategory DataFrames
+Extract and transform the crowdfunding.xlsx . Export the category DataFrame as category.csv  and the subcategory DataFrame as subcategory.csv and save it to your GitHub repository.
+
+* Export the campaign DataFrame as campaign.csv and save it to your GitHub repository.
+Create the Contacts DataFrame
+* Choose one of the following two options for extracting and transforming the data from the contacts.xlsx Excel data:
+Option 1: Use Python dictionary methods.
+Option 2: Use regular expressions.
+
+
+Create the Crowdfunding Database
+- Inspect the four CSV files, and then sketch an ERD of the tables by using QuickDBD
+Use the information from the ERD to create a table schema for each CSV file.
+Note: Remember to specify the data types, primary keys, foreign keys, and other constraints.
+- Save the database schema as a Postgres file named crowdfunding_db_schema.sql, and save it to your GitHub repository.
+- Create a new Postgres database, named crowdfunding_db.
+- Using the database schema, create the tables in the correct order to handle the foreign keys.
+- Verify the table creation by running a SELECT statement for each table.
+- Import each CSV file into its corresponding SQL table.
+Verify that each table has the correct data by running a SELECT statement for each.
---
+++ crowdfunding_db_schema.sql
@@ -0,0 +1,60 @@
+﻿-- Dropping Tables--
+DROP TABLE IF EXISTS campaign;
+DROP TABLE IF EXISTS contacts;
+DROP TABLE IF EXISTS category;
+DROP TABLE IF EXISTS subcategory;
+
+-- Creating Tables--
+CREATE TABLE "contacts" (
+    "contact_id" INT   NOT NULL,
+    "first_name" VARCHAR(20)   NOT NULL,
+    "last_name" VARCHAR(20)   NOT NULL,
+    "email" VARCHAR(100)   NOT NULL,
+    CONSTRAINT "pk_contacts" PRIMARY KEY ("contact_id")
+);
+SELECT * FROM contacts;
+-----------------------------------------
+CREATE TABLE "category" (
+    "category_id" VARCHAR(20)   NOT NULL,
+    "category" VARCHAR(20)   NOT NULL,
+    CONSTRAINT "pk_category" PRIMARY KEY ("category_id")
+);
+SELECT * FROM category;
+-----------------------------------------
+CREATE TABLE "subcategory" (
+    "subcategory_id" VARCHAR(20)   NOT NULL,
+    "subcategory" VARCHAR(20)   NOT NULL,
+    CONSTRAINT "pk_subcategory" PRIMARY KEY ("subcategory_id")
+);
+SELECT * FROM subcategory;
+-----------------------------------------
+CREATE TABLE "campaign" (
+    "cf_id" INT   NOT NULL,
+    "contact_id" INT   NOT NULL,
+    "company_name" VARCHAR(100)   NOT NULL,
+    "description" VARCHAR(100)   NOT NULL,
+    "goal" DECIMAL(13,2)  NOT NULL,
+    "pledged" DECIMAL(13,2)  NOT NULL,
+    "outcome" VARCHAR(20)   NOT NULL,
+    "backers_count" INT   NOT NULL,
+    "country" VARCHAR(20)   NOT NULL,
+    "currency" VARCHAR(20)   NOT NULL,
+    "launch_date" DATE   NOT NULL,
+    "end_date" DATE   NOT NULL,
+    "category_id" VARCHAR(20)   NOT NULL,
+    "subcategory_id" VARCHAR(20)   NOT NULL,
+    CONSTRAINT "pk_campaign" PRIMARY KEY (
+        "cf_id"
+     )
+);
+SELECT * FROM campaign;
+
+ALTER TABLE "campaign" ADD CONSTRAINT "fk_campaign_contact_id" FOREIGN KEY("contact_id")
+REFERENCES "contacts" ("contact_id");
+
+ALTER TABLE "campaign" ADD CONSTRAINT "fk_campaign_category_id" FOREIGN KEY("category_id")
+REFERENCES "category" ("category_id");
+
+ALTER TABLE "campaign" ADD CONSTRAINT "fk_campaign_subcategory_id" FOREIGN KEY("subcategory_id")
+REFERENCES "subcategory" ("subcategory_id");
+
